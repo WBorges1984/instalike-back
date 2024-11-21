f7c3abbfc1bc3587fd224fa68445417354991ea5
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2024-11-21 16:42:41 -0300

    IDEALIZANDO

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,32 @@
-import { getAllPosts } from "../models/postsModel.js";
+import { getAllPosts, criarPost } from "../models/postsModel.js";
 
 export async function listarPosts (req, res){
     const posts = await getAllPosts();
     res.status(200).json(posts);
+}
+
+export async function postarNovoPost(req, res){
+    const novoPost = req.body;
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado );
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+export async function uploadImagem(req, res){
+    const novoPost = req.body;
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado );
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
 }
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,10 +1,15 @@
 import conectarAoBanco from "../config/dbConfig.js";
 
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export async function getAllPosts(){
-    const db = conexao.db('imersao-instabytes');
-    const colecao = db.collection('posts');
-    return colecao.find().toArray();
-  }
-  
+export async function getAllPosts() {
+  const db = conexao.db("imersao-instabytes");
+  const colecao = db.collection("posts");
+  return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+  const db = conexao.db("imersao-instabytes");
+  const colecao = db.collection("posts");
+  return colecao.insertOne(novoPost)
+}
